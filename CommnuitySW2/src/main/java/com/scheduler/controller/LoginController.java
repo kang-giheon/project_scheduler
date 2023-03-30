154eb6dd7da732100e19e7a5f7f653d74ef20a1e
JuYeon <zoe4121@naver.com>
2023-03-30 10:27:04 +0900

    join, login, findID, findPW 추가/수정

--- CommnuitySW2/src/main/java/com/scheduler/controller/LoginController.java
+++ CommnuitySW2/src/main/java/com/scheduler/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.scheduler.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,4 +14,15 @@ public class LoginController {
 		
 		return "login";
 	}
+	
+    @GetMapping("/loginfailed")  
+    public String loginerror(Model model) {  
+        model.addAttribute("error", "true");  
+        return "login"; 
+    } 
+    
+    @GetMapping("/logout")
+    public String logout(Model model) {  
+        return "login";  
+    } 
 }
